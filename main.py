4a4a1ecdf475652fec4ec794a8edda0355dbef9e
Manorath <chughmanorath@gmail.com>
2025-05-21 11:00:19 +0530

    first change

---
+++ main.py
@@ -0,0 +1,23 @@
+from scrape_website import scrape
+from rag import setup_and_run_rag_pipeline
+
+RED = '\033[91m'
+GREEN = '\033[92m'
+RESET = '\033[0m'
+
+def main():
+    # Step 1: Run the web crawler to scrape the website
+    # url = 'https://docs.chaicode.com/'
+    # scrape(start_url=url)
+    
+    # Step 2: Process the scraped data using the RAG pipeline
+    question = "can you provid a list of all the functions in the chaicode library"
+    answer = setup_and_run_rag_pipeline(
+        question=question,
+    )
+    
+    print(f"{RED}Question: {question}{RESET}")
+    print(f"{GREEN}Answer: {answer}{RESET}")
+
+if __name__ == "__main__":
+    main()
---
+++ scrape_website.py
@@ -0,0 +1,52 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from urllib.parse import urlparse, urljoin
+import logging
+
+
+class WebsiteSpider(scrapy.Spider):
+    name = 'website'
+
+    def __init__(self, start_url=None, *args, **kwargs):
+        super(WebsiteSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [start_url]
+        self.allowed_domains = [urlparse(start_url).netloc]
+
+    def parse(self, response):
+        yield {
+            'url': response.url,
+            'title': response.css('title::text').get(),
+            'content': ' '.join(response.css('p::text').getall()),
+        }
+
+        for href in response.css('a::attr(href)'):
+            url = urljoin(response.url, href.get())
+            if url.startswith('http'):
+                yield response.follow(url, self.parse)
+
+
+def scrape(start_url, output_file='output.json'):
+    """
+    Run the WebsiteSpider with the given parameters.
+
+    :param start_url: The starting URL for the spider.
+    """
+    
+    logging.getLogger('scrapy').setLevel(logging.CRITICAL)
+
+    process = CrawlerProcess(settings={
+        'FEEDS': {
+            output_file: {
+                'format': 'json',
+                'overwrite': True
+            },
+        },
+        'LOG_LEVEL': 'CRITICAL',
+        'ROBOTSTXT_OBEY': True,
+        'CONCURRENT_REQUESTS': 8,
+        'DOWNLOAD_DELAY': 1,
+        'DEPTH_LIMIT': 5,
+    })
+
+    process.crawl(WebsiteSpider, start_url=start_url)
+    process.start()
