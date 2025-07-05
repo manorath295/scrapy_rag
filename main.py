2150b5e78ec6ae6e52dca4d13736c51b81a9bd3f
manorath <143715185+manorath295@users.noreply.github.com>
2025-07-05 23:09:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from rag import setup_and_run_rag_pipeline
 
 def main():
     
-    scrape("https://docs.chaicode.com/youtube/getting-started/")
+    scrape("website url")
 
     # 2. Ask a question
-    question = "What this post is about whatit want to tell i am providing you scrpping lonk why are oyu saying i dont have contex tto answer this wuestion  https://x.com/Life_of_coder/status/1932120974620823688 this is url"
+    question = "Here Ask The Qestion"
     answer = setup_and_run_rag_pipeline(question)
 
     print(f"\n❓ Question: {question}\n✅ Answer: {answer}\n")
